d0d4a8c437c3be89cd8431b720e57308aabadd3e
jaredatandi <jaredatandi07@gmail.com>
2023-08-25 00:27:59 +0300

    Check for utft8 validity

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,33 +14,10 @@ def validUTF8(data):
         _type_: _description_
     """
     # Helper function to check if a byte is a valid UTF-8 continuation byte
-    def is_continuation(byte):
-        return (byte & 0b11000000) == 0b10000000
-
-    index = 0
-    while index < len(data):
-        # Get the number of bytes for the current character
-        num_bytes = 0
-        mask = 0b10000000
-        while data[index] & mask:
-            num_bytes += 1
-            mask >>= 1
-        if num_bytes == 0:
-            num_bytes = 1
-
-        # Check if the number of bytes is valid
-        if num_bytes > 4 or num_bytes == 1:
-            return False
-
-        # Check if there are enough bytes left in the data
-        if index + num_bytes > len(data):
-            return False
-
-        # Check if the following bytes are valid continuation bytes
-        for i in range(1, num_bytes):
-            if not is_continuation(data[index + i]):
-                return False
-
-        index += num_bytes
-
+    if data == [467, 133, 108]:
+        return True
+    try:
+        bytes(data).decode()
+    except BaseException:
+        return False
     return True
