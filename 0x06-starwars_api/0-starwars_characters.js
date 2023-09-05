9eb9e69c39f01d8dcdb21cf94b77f3d7011b6b03
jaredatandi <jaredatandi07@gmail.com>
2023-09-05 20:57:26 +0300

    Remove semistandard errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,34 +6,34 @@ const request = require('request-promise'); // Use 'request-promise' for Promise
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.error('Please provide a movie ID as the first argument.');
-    process.exit(1);
+  console.error('Please provide a movie ID as the first argument.');
+  process.exit(1);
 }
 
 // Define the Star Wars API URL for fetching movie details
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 // Function to fetch and print character names
-async function fetchAndPrintCharacterNames() {
-    try {
-        const movie = await request(apiUrl, { json: true });
+async function fetchAndPrintCharacterNames () {
+  try {
+    const movie = await request(apiUrl, { json: true });
 
-        if (movie && movie.characters && movie.characters.length > 0) {
-            const characterPromises = movie.characters.map((characterUrl) => {
-                return request(characterUrl, { json: true });
-            });
+    if (movie && movie.characters && movie.characters.length > 0) {
+      const characterPromises = movie.characters.map((characterUrl) => {
+        return request(characterUrl, { json: true });
+      });
 
-            const characters = await Promise.all(characterPromises);
+      const characters = await Promise.all(characterPromises);
 
-            characters.forEach((character) => {
-                console.log(`${character.name}`);
-            });
-        } else {
-            console.log(`No characters found for "${movie.title}".`);
-        }
-    } catch (error) {
-        console.error('Error:', error.message);
+      characters.forEach((character) => {
+        console.log(`${character.name}`);
+      });
+    } else {
+      console.log(`No characters found for "${movie.title}".`);
     }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
 // Call the function to fetch and print character names
