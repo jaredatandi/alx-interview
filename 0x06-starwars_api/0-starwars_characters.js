254c2c5fddc17a41d75a35f172fc23f415be0542
jaredatandi <jaredatandi07@gmail.com>
2023-09-05 20:56:12 +0300

    Use promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,48 +1,39 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request-promise'); // Use 'request-promise' for Promise-based requests
 
-// confirm input
+// Confirm input
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.error('Please provide a movie ID as the first argument.');
-  process.exit(1);
+    console.error('Please provide a movie ID as the first argument.');
+    process.exit(1);
 }
 
 // Define the Star Wars API URL for fetching movie details
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 // Function to fetch and print character names
-function fetchAndPrintCharacterNames () {
-  request(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      if (response.statusCode === 200) {
-        const movie = JSON.parse(body);
-        const characters = movie.characters;
+async function fetchAndPrintCharacterNames() {
+    try {
+        const movie = await request(apiUrl, { json: true });
 
-        if (characters && characters.length > 0) {
-          characters.forEach((characterUrl) => {
-            // Fetch character details
-            request(characterUrl, (charError, charResponse, charBody) => {
-              if (!charError && charResponse.statusCode === 200) {
-                const character = JSON.parse(charBody);
-                console.log(`${character.name}`);
-              } else {
-                console.error('Error fetching character details:', charError);
-              }
+        if (movie && movie.characters && movie.characters.length > 0) {
+            const characterPromises = movie.characters.map((characterUrl) => {
+                return request(characterUrl, { json: true });
             });
-          });
+
+            const characters = await Promise.all(characterPromises);
+
+            characters.forEach((character) => {
+                console.log(`${character.name}`);
+            });
         } else {
-          console.log(`No characters found for "${movie.title}".`);
+            console.log(`No characters found for "${movie.title}".`);
         }
-      } else {
-        console.error('Request failed with status code:', response.statusCode);
-      }
+    } catch (error) {
+        console.error('Error:', error.message);
     }
-  });
 }
 
 // Call the function to fetch and print character names
